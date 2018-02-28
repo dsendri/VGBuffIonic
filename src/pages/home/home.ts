416ba0923193d2f4b47bc0c2c76fe226c04fb5d4
dsendri <dsendri@gmail.com>
2018-02-28 13:18:46 +0700

    add ui

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,19 +9,27 @@ import { VgapiProvider } from '../../providers/vgapi/vgapi';
 export class HomePage {
 
   playerSummary: any;
-  serverloc = 'sg';
-  ignName = 'santadoge';
+  serverloc = '';
+  ignName = '';
 
   constructor(public navCtrl: NavController, private vgapiProvider:VgapiProvider) {
 
   }
 
   ionViewWillEnter(){
+    
+  }
+
+  search(){
+    console.log(this.serverloc);
+    console.log(this.ignName);
+
     this.vgapiProvider.getPlayerSummary(this.serverloc, this.ignName)
       .subscribe(playerSummary => {
         console.log(playerSummary.data[0].attributes.name);
         this.playerSummary = playerSummary.data[0];
       });
+      
   }
 
 }
